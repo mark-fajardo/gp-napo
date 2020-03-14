ea785bc5c10f2f6827e32ffbb4b4b65befabd0fe
Mark Joshua Tolentino Fajardo <mjt.fajardo@gmail.com>
2020-03-14 22:12:58 +0800

    feat: updated sql

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Repositories\Items;
 
 class HomeController extends Controller
 {
     
     public function index() 
     {
+        // $oItems = Items::where('updated_at', '>', Carbon::now()->subMonths(12))->groupBy('updated_at')->get();
+        $oItems = Items::selectRaw('SUM(`views`)')
+        ->where('updated_at', '>', Carbon::now()->subMonths(12))->groupBy('updated_at')
+        ->groupBy()
+        ->get();
+        dd($oItems);
         return view('admin.home.index');
     }
 
